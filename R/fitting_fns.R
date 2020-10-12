7ec9d3acf9530d79fdd5d04cd7a9db243edb9b97
justjacqueline <jacquelinejtay@gmail.com>
2020-10-12 18:22:04 -0400

    Added cost function to its own file.

---
+++ R/fitting_fns.R
@@ -0,0 +1,15 @@
+#' @title Runs the model and calculates the cost
+#'
+#' @description Runs the model and calculates the cost
+#' @param p
+#' @param dat
+#' @param times_out
+#' @param model
+#' @param initial
+#' @return The model cost
+#' @export
+#' @examples #
+#'
+cost <- function(p, dat, times_out, model, initial) {
+  out <- ode(initial, times_out, model, p)
+  modCost(out, dat, weight = "none")} # try weight = "std" or "mean"
